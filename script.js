5af44ed487c17e3566da94e252366577c6b0fdfc
abhinay mhatre <abhinaymhatre1234@gmail.com>
2026-02-17 00:07:09 +0530

    fixed corousel

--- script.js
+++ script.js
@@ -21,28 +21,50 @@ function hoveredline() {
   });
 }
 
-function rotatingLogo(){
- let socials = document.querySelectorAll('.social-icon');
-console.log('hitting')
-console.log(socials)
- socials.forEach((elem) => {
-    elem.addEventListener("mouseenter",()=>{
-      console.log(elem)
-        gsap.to(elem.querySelector(".i"),{
-           rotationY:360,
-           duration:0.5 
-        })
+function rotatingLogo() {
+  let socials = document.querySelectorAll(".social-icon");
+  console.log("hitting");
+  console.log(socials);
+  socials.forEach((elem) => {
+    elem.addEventListener("mouseenter", () => {
+      console.log(elem);
+      gsap.to(elem.querySelector(".i"), {
+        rotationY: 360,
+        duration: 0.5,
+      });
     });
 
-     elem.addEventListener("mouseleave",()=>{
-      console.log(elem)
-        gsap.to(elem.querySelector(".i"),{
-           rotationY:0,
-           duration:0.5
-        })
-    })
- })
+    elem.addEventListener("mouseleave", () => {
+      console.log(elem);
+      gsap.to(elem.querySelector(".i"), {
+        rotationY: 0,
+        duration: 0.5,
+      });
+    });
+  });
 }
+
+const scrollAniamtion = gsap.to(".main-text", {
+  x: "-100%",
+  duration: 50,
+  ease: "none",
+  repeat: -1,
+ 
+});
+
+// gsap.to(scrollAniamtion,{
+//   timeScale:5,
+//   ease:"power2.inOut",
+//   scrollTrigger:{
+//     trigger:'.main-content',
+//    markers: true,
+//     scroller: "body",
+//     start: 'top 10%',
+//     end: 'bottom -100%',
+   
+//   }
+// })
+
 
 rotatingLogo();
 hoveredline();
