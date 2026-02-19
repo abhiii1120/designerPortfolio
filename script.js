b09674d4fdd2c705f0b8272590651216561bb3a1
abhinay mhatre <abhinaymhatre1234@gmail.com>
2026-02-20 00:10:48 +0530

    intro desktop screen ui compeleted

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const lenis = new Lenis({
   duration: 1.5,
   smoothWheel: true, // smooth mouse wheel
@@ -81,31 +80,90 @@ const scrollAniamtion = gsap.to(".main-text", {
 });
 
 const t2 = gsap.timeline({
-  scrollTrigger:{
-    trigger:'.reveal-text-container',
-    start:'top 70%',
-    end:'bottomv 30%',
-    scrub:1,
+  scrollTrigger: {
+    trigger: ".reveal-text-container",
+    start: "top 70%",
+    end: "bottomv 30%",
+    scrub: 1,
+  },
+});
+
+const whiteText = new SplitType(".white-text", {
+  types: "chars",
+  tagName: "span",
+});
+const whiteText2 = new SplitType(".white-text2", {
+  types: "chars",
+  tagName: "span",
+});
+const orangeText = new SplitType(".orange-text", {
+  types: "chars",
+  tagName: "span",
+});
+
+gsap.set(".char", { display: "inline" }); // this is the magic line
+
+t2.fromTo(
+  whiteText.chars,
+  { color: "#222" },
+  { color: "#ffffff", stagger: 1.25 },
+)
+  .fromTo(
+    orangeText.chars,
+    { color: "#222" },
+    { color: "#f05038", stagger: 1.25 },
+  )
+  .fromTo(
+    whiteText2.chars,
+    { color: "#222" },
+    { color: "#ffffff", stagger: 1.25 },
+  )
+  .from(
+    '.secondary-text, .button-container',{
+      opacity:0,
+      duration:1,
+      ease:'slow(0.7,0.7,false)'
+    }
+  )
+
+  function animatedButton(){
+    const button = document.querySelectorAll('.button-container');
+    button.forEach(elem =>{
+      elem.addEventListener('mouseenter',()=>{
+        gsap.to(elem.querySelector('.initial-text'),{
+          top:'-50px',
+          position:'absolute',
+          duration:0.25
+        })
+        gsap.to(elem.querySelector('.below-text'),{
+          bottom:0,
+          position:'relative',
+          duration:0.25
+        })
+         gsap.to(elem.querySelector('.animated-color'),{
+          height:'400%',
+          width:'150%',
+        })
+      })
+      elem.addEventListener('mouseleave',()=>{
+        gsap.to(elem.querySelector('.initial-text'),{
+          top:'0px',
+          position:'relative',
+          duration:0.25
+        })
+       gsap.to(elem.querySelector('.below-text'),{
+          bottom:'-50px',
+          position:'absolute',
+          duration:0.25
+        })
+           gsap.to(elem.querySelector('.animated-color'),{
+          height:'0%',
+          width:'0%',
+        })
+      })
+    })
   }
-})
 
-const whiteText = new SplitType('.white-text',{types:'chars'});
-const whiteText2 = new SplitType('.white-text2',{types:'chars'});
-const orangeText = new SplitType('.orange-text',{types:'chars'});
-
-
-t2.fromTo(whiteText.chars,
-  { color: '#222' },
-  { color: '#ffffff', stagger: 0.75 }
-)
-.fromTo(orangeText.chars,
-  { color: '#222' },
-  { color: 'rgb(255, 60, 0)', stagger: 0.75 }
-)
-.fromTo(whiteText2.chars, 
-  { color: '#222' },
-  { color: '#ffffff', stagger: 0.75 }
-)
-
-rotatingLogo();
-hoveredline();
+  rotatingLogo();
+  hoveredline();
+  animatedButton();
