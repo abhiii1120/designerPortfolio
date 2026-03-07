35b51e29d96ce010c6ca4f5d58fa70a3bbada6d4
abhinay mhatre <abhinaymhatre1234@gmail.com>
2026-03-07 13:10:31 +0530

    footer and testimonials done both desktop and mobile ui

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const lenis = new Lenis({
 });
 const mm = gsap.matchMedia();
 
-
 gsap.ticker.add((time) => {
   lenis.raf(time * 1000);
 });
@@ -30,9 +29,11 @@ t1.from(".initial-page .desktop-image,.mobile-image", {
     },
   );
 
+const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => document.querySelectorAll(selector);
+
 function hoveredline() {
   let hoverUnderline = document.querySelectorAll(".hover-underline");
-
   hoverUnderline.forEach((element) => {
     element.addEventListener("mouseenter", () => {
       gsap.to(element.querySelector(".line"), {
@@ -102,30 +103,33 @@ const orangeText = new SplitType(".orange-text", {
   tagName: "span",
 });
 
-gsap.set(".char", { display: "inline" }); // this is the magic line
+gsap.set(".char", { display: "inline" });
 
 function buildTimeline() {
   t2.fromTo(
-      whiteText.chars,
-      { color: "#222" },
-      { color: "#ffffff", stagger: 1.25, duration: 1 }
-    )
+    whiteText.chars,
+    { color: "#222" },
+    { color: "#ffffff", stagger: 1.25, duration: 1 },
+  )
     .fromTo(
       orangeText.chars,
       { color: "#222" },
       { color: "#f05038", stagger: 1.25, duration: 1 },
-      
     )
     .fromTo(
       whiteText2.chars,
       { color: "#222" },
       { color: "#ffffff", stagger: 1.25, duration: 1 },
     )
-    .from(".secondary-text, .button-container", {
-      opacity: 0,
-      duration: 1,
-      ease: "slow(0.7,0.7,false)",
-    }, "<-=1");
+    .from(
+      ".secondary-text, .button-container",
+      {
+        opacity: 0,
+        duration: 1,
+        ease: "slow(0.7,0.7,false)",
+      },
+      "<-=1",
+    );
 }
 function animatedButton() {
   const button = document.querySelectorAll(".button-container");
@@ -179,12 +183,12 @@ function animatedProjectsSection() {
         invalidateOnRefresh: true,
       },
       scale: 0.7,
-      opacity: 0, 
+      opacity: 0,
     });
   });
- window.addEventListener("resize", () => {
-    ScrollTrigger.invalidate(); // clears cached values
-    ScrollTrigger.refresh();    // recalculates everything fresh
+  window.addEventListener("resize", () => {
+    // ScrollTrigger.invalidate(); // clears cached values
+    ScrollTrigger.refresh(); // recalculates everything fresh
   });
 }
 
@@ -209,9 +213,180 @@ function mouseFollower() {
     });
   });
 }
+
+const projectArray = [
+  {
+    year: 2020,
+    heading: "Formula Vintage",
+    desc: "For Formula Vintage, we crafted a design that honors the rich heritage of classic cars while adding a modern twist. Combining timeless elegance with sleek, contemporary elements, we created an experience that appeals to both enthusiasts and newcomers, celebrating the past with a fresh perspective.",
+    img: "https://framerusercontent.com/images/EHSkKBS5f12yNXJu5VZ8VAMv4.png",
+    firstPreset: "landing Page",
+    secondPreset: "Mobile App",
+    thirdPreset: "Redesign",
+  },
+  {
+    year: 2024,
+    heading: "Sprey Zest",
+    desc: ` For Sprey Zest, we took a playful, bold
+              approach to packaging and branding. Instead
+              of following the typical fresh or clean
+              aesthetic, we infused energy and personality
+              into every detail, making the product stand
+              out of shelves and bringing a burst of
+              excitement to the consumer experience.`,
+    img: "https://framerusercontent.com/images/BUMWJfM7FAxHoEYXhMjW881wM.png",
+    firstPreset: "Website Design",
+    secondPreset: "Branding",
+  },
+  {
+    year: 2020,
+    heading: "Super Pro",
+    desc: `For Super-Pro, we redefined what it means to
+              be a professional by focusing on the mindset
+              and determination behind success, not just
+              the achievements. the design highlighted the
+              drive and passion of athletes, creating a bold,
+              empowering experience the resonated with
+              anyone striving for excellence.`,
+    img: "https://framerusercontent.com/images/VQY4WQ8E6fx261RhsoqM17bkt9E.png",
+    firstPreset: "Desktop App",
+    secondPreset: "Mobile App",
+  },
+  {
+    year: 2024,
+    heading: "Architech Buildings",
+    desc: ` we redefined the comcept of modern living
+              by creating a design that challenges
+              conventional boundaries. Focusing on 
+              comfort, functionality, and unexpected
+              elements, we transformed the ordinary into
+              something extraordinary, making everyday
+              experiences feel fresh and unique.`,
+    img: "https://framerusercontent.com/images/Z1w2O7B5xsN3Y7XkKgBi9kFOgps.jpg",
+    firstPreset: "Mobile App",
+    secondPreset: "Branding",
+    thirdPreset: "Website Design",
+  },
+];
+const projectContainer = $(".project-container");
+
+function injectProjects() {
+  projectContainer.innerHTML = projectArray.map((item) => {
+    return `   <div class="project">
+        <div class="project-image">
+          <img
+            src="${item.img}"
+            alt=""
+          />
+        </div>
+        <div class="project-info">
+          <div class="first-container">
+            <div class="year">(${item.year})</div>
+            <div class="heading">${item.heading}</div>
+            <div class="sub-heading">
+             ${item.desc}
+            </div>
+          </div>
+          <div class="second-container">
+            <div class="landing-page">${item.firstPreset}</div>
+            <div class="line-div"></div>
+            <div class="mobile-app lineY">${item.secondPreset}</div>
+            ${item?.thirdPreset ? '<div class="line-div"></div>' : ""}
+            <div class="redesign">${item?.thirdPreset || ""}</div>
+          </div>
+        </div>
+      </div>`;
+  });
+}
+
+const t3 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".testimonial",
+    top: "top 70%",
+    end: "end 30%",
+    scrub: 1,
+  },
+});
+
+function revealTestamonials() {
+  const testamonialsOrangeText = new SplitType(".testimonials-orange-text", {
+    types: "chars",
+    tagName: "span",
+  });
+  const testamonialsWhiteText = new SplitType(".testimonials-white-text", {
+    types: "chars",
+    tagName: "span",
+  });
+
+  t3.fromTo(
+    testamonialsWhiteText.chars,
+    { color: "#222" },
+    { color: "#ffffff", stagger: 1.25, duration: 1 },
+  ).fromTo(
+    testamonialsOrangeText.chars,
+    { color: "#222" },
+    { color: "#f05038", stagger: 1.25, duration: 1 },
+  );
+  gsap.set(".char", { display: "inline" });
+}
+
+const testimonials = [
+  {
+    number: "01",
+    whiteText: '"His keen eye for detail and innovative approach',
+    orangeText: ` impressed our team, turning
+                challenges into creative solutions that set him apart."`,
+    name: "//Maya Lopez",
+    designation: "CEO, fundwizz",
+  },
+   {
+    number: "02",
+    whiteText: '"His strong problem-solving skills and dedication to excellence consistently elevated our projects,',
+    orangeText: ` making a lasting impact on the team."`,
+    name: "//George Jones",
+    designation: "Product Manager, Gliss",
+  },
+   {
+    number: "03",
+    whiteText: '"With a unique blend of creativity and precision,',
+    orangeText: ` he brought fresh perspectives that drove meaningful results."`,
+    name: "//Ray Brown",
+    designation: "Head of Product, ISO",
+  },
+];
+
+function onClickTestimonial(){
+  const imageCards = $$('.image-1');
+  const number = $('.orange-number');
+  const testimonialWhiteText = $('.testimonials-white-text');
+  const testimonialOrangeText = $('.testimonials-orange-text');
+  const nameContainer = $('.name-container');
+
+  imageCards.forEach((card)=>{
+    card.addEventListener("click",()=>{
+      imageCards.forEach(c => c.classList.remove("active"));
+      card.classList.add("active");
+      const index = card.dataset.index;
+      const data = testimonials[index];
+
+      number.innerHTML = data.number;
+      testimonialWhiteText.innerText = data.whiteText;
+      testimonialOrangeText.innerHTML = data.orangeText
+      testimonialOrangeText.style.color = '#f05038'
+      testimonialWhiteText.style.color = '#fff'
+      nameContainer.innerHTML =` ${data.name}  <span class="designation">${data.designation}</span>`
+      revealTestimonials()
+    })
+  })
+}
+
+
 rotatingLogo();
 hoveredline();
 animatedButton();
+injectProjects();
 animatedProjectsSection();
 mouseFollower();
 buildTimeline();
+onClickTestimonial();
+revealTestamonials();
